a2fa9386b19a4ad77aa17edb548e357a85b5e05d
Vaquita <vaquita@savethevaquitas.com>
2020-02-07 09:40:25 +0000

    update from internal git

---
+++ js/timeline.js
@@ -0,0 +1,23 @@
+/* Scripts for the timeline page */
+
+var currentlySelected;
+
+var timelineItems = [
+    {
+        "testname1": {
+            "id" : "0",
+            "date":"01/01/1970",
+            "description" : "This is test 1, Very long description goes here."
+
+        },
+        "testname2": {
+            "id" : "1",
+            "date":"02/03/2005",
+            "description" : "This is test 2, Very long description goes here."
+        }
+    }
+];
+
+function onButtonClickRaise(desId) {
+    // This will "raise" the text
+}
